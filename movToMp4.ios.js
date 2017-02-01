dd64a6d74437c0c8152cbaed0726b19a85692c51
talc <tal.cohen@wearetv.com>
2017-02-01 17:07:20 +0200

    update file

--- movToMp4.ios.js
+++ movToMp4.ios.js
@@ -2,10 +2,19 @@
  * @providesModule movToMp4
  * @flow
  */
-'use strict';
+import React, { Component, PropTypes } from 'react';
+import {
+    DeviceEventEmitter, // android
+    NativeAppEventEmitter, // ios
+    NativeModules,
+    Platform,
+    StyleSheet,
+    requireNativeComponent,
+    View,
+} from 'react-native';
 
-var NativemovToMp4 = require('NativeModules').movToMp4;
-
+//var NativemovToMp4 = require('NativeModules').movToMp4;
+var NativemovToMp4 = NativeModules.movToMp4;
 /**
  * High-level docs for the movToMp4 iOS API can be written here.
  */
