8ae0e397f6ff9b4a386638a9cbeacb3e589d7bdd
Jason Ashby <jashby2@gmail.com>
2020-02-19 16:23:09 -0500

    git added index files

---
+++ index.ios.js
@@ -0,0 +1,19 @@
+/**
+ * @providesModule movToMp4
+ * @flow
+ */
+import {
+    NativeModules,
+} from 'react-native';
+
+//var NativemovToMp4 = require('NativeModules').movToMp4;
+var NativemovToMp4 = NativeModules.movToMp4;
+/**
+ * High-level docs for the movToMp4 iOS API can be written here.
+ */
+
+var movToMp4 = {
+  convertMovToMp4: NativemovToMp4.convertMovToMp4,
+};
+
+export default movToMp4;
